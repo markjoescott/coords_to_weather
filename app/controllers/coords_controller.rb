be457f22ca44d411ccbaa3cb44de44a707dfb659
markjoescott <m-scott@kellogg.northwestern.edu>
2014-05-03 12:13:46 -0500

    Started working on coords_to_weather
    Got the temp. to show correctly in phase one, but the summaries don’t
    seem to change…

--- app/controllers/coords_controller.rb
+++ app/controllers/coords_controller.rb
@@ -5,15 +5,15 @@ class CoordsController < ApplicationController
   def fetch_weather
     @latitude = 42.0538387
     @longitude = -87.67721
-    your_api_key = ""
+    your_api_key = "28d93f8e38dfa9d0a4ecbfc541f1df61"
 
     # Your code goes here.
-    # url = ?
-    # raw_data = ?
-    # parsed_data = ?
-    # @temperature = ?
-    # @minutely_summary = ?
-    # @hourly_summary = ?
-    # @daily_summary = ?
+    url = "https://api.forecast.io/forecast/28d93f8e38dfa9d0a4ecbfc541f1df61/37.8267,-122.423"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    @temperature = parsed_data["currently"]["temperature"]
+    @minutely_summary = parsed_data["minutely"]["summary"]
+    @hourly_summary = parsed_data["hourly"]["summary"]
+    @daily_summary = parsed_data["daily"]["summary"]
   end
 end
