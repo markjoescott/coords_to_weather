d456b667b143abfaa331f1c79eef82a37479e004
markjoescott <m-scott@kellogg.northwestern.edu>
2014-05-05 13:02:07 -0500

    Finished coords_to weather
    Added params and string interpolation to successfully get the coords to
    weather to work

--- app/controllers/coords_controller.rb
+++ app/controllers/coords_controller.rb
@@ -3,12 +3,29 @@ require 'json'
 
 class CoordsController < ApplicationController
   def fetch_weather
-    @latitude = 42.0538387
-    @longitude = -87.67721
+    @address = params[:address]
+    if @address == nil
+      @url_safe_address = URI.encode("the corner of Foster and Sheridan")
+    else
+      @url_safe_address = URI.encode(@address)
+    end
+
+    # Your code goes here.
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_address}+&sensor=false"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    if parsed_data["results"][0] != nil
+        @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+        @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+    else
+        @latitude = 0.0
+        @longitude = 0.0
+    end
+
     your_api_key = "28d93f8e38dfa9d0a4ecbfc541f1df61"
 
     # Your code goes here.
-    url = "https://api.forecast.io/forecast/28d93f8e38dfa9d0a4ecbfc541f1df61/37.8267,-122.423"
+    url = "https://api.forecast.io/forecast/28d93f8e38dfa9d0a4ecbfc541f1df61/#{@latitude},#{@longitude}"
     raw_data = open(url).read
     parsed_data = JSON.parse(raw_data)
     @temperature = parsed_data["currently"]["temperature"]
